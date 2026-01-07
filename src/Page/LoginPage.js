5d1560a62c8460567ac55659037b6ff59da86b86
salma <diouanesalma79@gmail.com>
2026-01-07 22:01:45 +0100

    Initial commit for HomeFlavors LoginPage

---
+++ src/Page/LoginPage.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './LoginPage.css'; 
+
+export const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2  className="fw-bold text-center mb-4">HomeFlavors</h2>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
+
